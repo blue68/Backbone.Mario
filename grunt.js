04caaf5f1a4c60fdc1c15d84c887bc245bd4fce7
qtalen <qtalen@163.com>
2012-11-23 19:07:56 +0800

    daily commit

--- grunt.js
+++ grunt.js
@@ -1,6 +1,6 @@
 /*global module:false*/
 module.exports = function(grunt) {
-
+  	grunt.loadNpmTasks('grunt-rigger');
   // Project configuration.
   grunt.initConfig({
     pkg: '<json:package.json>',
@@ -12,18 +12,71 @@ module.exports = function(grunt) {
         ' Licensed <%= _.pluck(pkg.licenses, "type").join(", ") %> */'
     },
     lint: {
-      files: ['grunt.js', 'lib/**/*.js', 'test/**/*.js']
+      files: ['grunt.js', 'src/mario.*.js']
+    },
+    rig : {
+    	core_build : {
+    		src : ['<banner:meta.banner>', 'src/build/mario.core.js'],
+    		dest : 'lib/core/backbone.mario.js'
+    	},
+    	core_amd : {
+    		src : ['<banner:meta.banner>', 'src/build/amd.core.js'],
+    		dest : 'lib/core/amd/backbone.mario.js'
+    	},
+    	build : {
+    		src : ['<banner:meta.banner>', 'src/build/mario.js'],
+    		dest : 'lib/backbone.mario.js'
+    	},
+    	amd : {
+    		src : ['<banner:meta.banner>', 'src/build/amd.js'],
+    		dest : 'lib/amd/backbone.mario.js'
+    	}
     },
     min: {
-      dist: {
-        src: ['<banner:meta.banner>', '<config:concat.dist.dest>'],
-        dest: 'dist/<%= pkg.name %>.min.js'
+      core_standard : {
+      	src : ['<banner:meta.banner>', '<config:rig.core_build.dest>'],
+      	dest : 'lib/core/backbone.mario.min.js'
+      },
+      core_amd : {
+      	src : ['<banner:meta.banner>', '<config:rig.core_amd.dest>'],
+      	dest : 'lib/core/amd/backbone.mario.min.js'
+      },
+      standard : {
+      	src : ['<banner:meta.banner>', '<config:rig.build.dest>'],
+      	dest : 'lib/backbone.mario.min.js'
+      },
+      amd : {
+      	src : ['<banner:meta.banner>', '<config:rig.amd.dest>'],
+      	dest : 'lib/amd/backbone.mario.min.js'
+      }
+    },
+    jshint: {
+      options: {
+        curly: true,
+        eqeqeq: true,
+        immed: false,
+        latedef: true,
+        newcap: true,
+        noarg: true,
+        sub: true,
+        undef: true,
+        boss: true,
+        eqnull: true,
+        browser: true
+      },
+      globals: {
+        jQuery: true,
+        Backbone: true,
+        _: true,
+        Marionette: true,
+        $: true,
+        slice: true
       }
     },
     uglify: {}
   });
 
   // Default task.
-  grunt.registerTask('default', 'lint min');
+  grunt.registerTask('default', 'lint rig min');
 
 };
